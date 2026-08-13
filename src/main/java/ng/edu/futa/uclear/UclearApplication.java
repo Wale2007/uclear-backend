fd947fd0d77de182113a73caad6bed930da8a449
Wale2007 <wola77923@gmail.com>
2026-08-13 10:58:18 +0100

    Fix Render startup crash: lazy init, defer datasource, configurable DDL-auto

--- src/main/java/ng/edu/futa/uclear/UclearApplication.java
+++ src/main/java/ng/edu/futa/uclear/UclearApplication.java
@@ -2,10 +2,13 @@ package ng.edu.futa.uclear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class    UclearApplication {
+public class UclearApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UclearApplication.class, args);
+        new SpringApplicationBuilder(UclearApplication.class)
+            .lazyInitialization(true)   // beans init on first use, not at startup
+            .run(args);
     }
 }
